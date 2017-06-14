a1d88f915fb94e040ff565278782da11c7381b4f
Luke Bonaccorsi <luke@lukeb.co.uk>
2017-06-14 21:07:01 +0100

    Use jscad-includify now that it has been split out

--- index.js
+++ index.js
@@ -5,11 +5,10 @@
 const express = require(`express`);
 const fs = require(`fs`);
 const http = require(`http`);
-const path = require(`path`);
-const recast = require(`recast`);
 const chokidar = require(`chokidar`);
-const WebSocket = require('ws');
-const opn = require('opn');
+const WebSocket = require(`ws`);
+const opn = require(`opn`);
+const includify = require(`jscad-includify`)
 
 const app = express();
 
@@ -17,42 +16,17 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
 let watchPaths = [];
+let watcher;
 
 app.use(express.static(`${__dirname}/viewer`));
 
 app.get(`/model.jscad`, (req, res) => {
-    fs.readFile(process.argv[2], `utf8`, (err, file) => {
-        file = includeify(file)
+    includify.runFile(process.argv[2]).then(({code, includes}) => {
+        updateWatcher(includes);
         res.statusCode = 200;
-        res.end(file);
+        res.end(code);
     });
 });
-
-function includeify(script) {
-    const ast = recast.parse(script);
-    recast.visit(ast, {
-        visitExpressionStatement: function(p) {
-            if (
-                p.node.expression &&
-                p.node.expression.callee &&
-                p.node.expression.callee.name === 'include'
-            ) {
-                const filePath = `${path.dirname(process.argv[2])}/${p.node.expression.arguments[0].value}`;
-
-                watchPaths.push(filePath)
-
-                const file = fs.readFileSync(filePath, `utf8`);
-                const includeAst = recast.parse(file);
-
-                p.replace(...includeAst.program.body);
-            }
-
-            this.traverse(p);
-        }
-    });
-
-    return recast.print(ast).code;
-}
 
 fs.access(process.argv[2], fs.constants.R_OK, (err) => {
     if (err) {
@@ -60,23 +34,17 @@ fs.access(process.argv[2], fs.constants.R_OK, (err) => {
         return;
     }
 
-    const watcher = chokidar.watch(process.argv[2], {
+    watcher = chokidar.watch(process.argv[2], {
         ignoreInitial: true
     }).on(`all`, () => {
-        watcher.unwatch(watchPaths);
-
-        watchPaths = [];
-
-        fs.readFile(process.argv[2], `utf8`, (err, file) => {
-            file = includeify(file);
+        includify.runFile(process.argv[2]).then(({code, includes}) => {
+            updateWatcher(includes);
 
             wss.clients.forEach((client) => {
                 if (client.readyState === WebSocket.OPEN) {
-                  client.send(file);
+                  client.send(code);
                 }
             });
-
-            watcher.add(watchPaths);
         });
     });
 
@@ -84,3 +52,9 @@ fs.access(process.argv[2], fs.constants.R_OK, (err) => {
         opn(`http://localhost:3000`);
     })
 });
+
+function updateWatcher(newWatchPaths) {
+    watcher.unwatch(watchPaths);
+    watchPaths = newWatchPaths;
+    watcher.add(watchPaths);
+}
